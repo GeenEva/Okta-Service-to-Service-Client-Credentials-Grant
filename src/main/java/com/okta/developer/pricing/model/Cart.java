e7699826ac754bee6d3158bac4bbf59173e2922e
Eva van Geenen <evavangeenen@gmail.com>
2020-08-19 14:10:34 +0200

    Added DefaultPricingService class and PricingService interface

--- src/main/java/com/okta/developer/pricing/model/Cart.java
+++ src/main/java/com/okta/developer/pricing/model/Cart.java
@@ -8,12 +8,14 @@ import java.util.List;
 
 @Data
 public class Cart {
+
     private Integer id;
     private String customerId;
     private List<LineItem> lineItems = new ArrayList<>();
     private MonetaryAmount total;
 
     public void addLineItem(LineItem lineItem){
+
         this.lineItems.add(lineItem);
     }
 
---
+++ src/main/java/com/okta/developer/pricing/service/PricingService.java
@@ -0,0 +1,7 @@
+package com.okta.developer.pricing.service;
+
+import com.okta.developer.pricing.model.Cart;
+
+public interface PricingService {
+    Cart price(Cart cart);
+}
