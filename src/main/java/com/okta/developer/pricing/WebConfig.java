80e3953c63e516af6d2a02bd8eea67b524964cdb
Eva van Geenen <evavangeenen@gmail.com>
2020-08-19 14:27:07 +0200

    Configured Jackson Money Module in a WebConfig class

---
+++ src/main/java/com/okta/developer/pricing/WebConfig.java
@@ -0,0 +1,14 @@
+package com.okta.developer.pricing;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.jackson.datatype.money.MoneyModule;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public MoneyModule moneyModule(){
+        return new MoneyModule().withDefaultFormatting();
+    }
+}
